1845920d00541c02a2fbec73fd7ec73f95d0ca32
konlawat67116806 <tomguycrep@gmail.com>
2026-01-18 15:23:58 +0700

    add

--- app/api/v1/depression/route.py
+++ app/api/v1/depression/route.py
@@ -8,8 +8,3 @@ router = APIRouter()
 @router.post('/depression', tags=["Depression"])
 def assess_depression(data: DepressionRequest):
     return get_depression()
-
-@router.get('/info', tags=["Depression"])
-def info():
-    return {"service": "Depression Assessment API", "version":"1.0"}
-
